1ad8ebd99886abaea2cfb002e46409c1c0b6ccab
Moath-shakhatreh <moath.shakhatreh@gmail.com>
2023-08-30 17:17:02 +0000

    m

--- components/details/details.js
+++ components/details/details.js
@@ -1,49 +1,31 @@
 import React from 'react';
 export default function Details() {
     return (
-
-
-
-        <body className="font-sans antialiased bg-gray-200">
-            <div className="max-w-6xl mx-auto">
-                <div className="flex items-center justify-center min-h-screen">
-                    <div className="w-full max-w-sm px-3 py-6 sm:w-1/2 lg:w-1/3">
-                        <div className="overflow-hidden bg-white rounded-lg shadow-xl">
-                            <div
-                                className="h-56 p-4 bg-center bg-cover"
-                                style={{
-                                    backgroundImage:
-                                        'url(https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80)',
-                                }}
-                            >
-                                <div className="flex justify-end">
-                                    <svg
-                                        className="w-6 h-6 text-white fill-current"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        {/* SVG path */}
-                                    </svg>
-                                </div>
-                            </div>
-                            <div className="p-4">
-                                <p className="text-sm font-bold tracking-wide text-gray-700 uppercase">
-                                    Detached house • 5y old
-                                </p>
-                                <p className="text-3xl text-gray-900">$750,000</p>
-                                <p className="text-gray-700">742 Evergreen Terrace</p>
-                            </div>
-                            <div className="flex p-4 text-gray-700 border-t border-gray-300">
-                                {/* ... */}
-                            </div>
-                            <div className="px-4 pt-3 pb-4 bg-gray-100 border-t border-gray-300">
-                                {/* ... */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className="addProduct">
+        <div className="card">
+            <nav>
+                <svg className="arrow" version="1.1" viewBox="0 0 512 512" width="512px" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink">
+                    <polygon points="352,115.4 331.3,96 160,256 331.3,416 352,396.7 201.5,256 " stroke="#727272" />
+                </svg>
+                Back to all Plants
+                <svg className="heart" version="1.1" viewBox="0 0 512 512" width="512px" xmlSpace="preserve" stroke="#727272" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink">
+                    <path d="M340.8,98.4c50.7,0,91.9,41.3,91.9,92.3c0,26.2-10.9,49.8-28.3,66.6L256,407.1L105,254.6c-15.8-16.6-25.6-39.1-25.6-63.9  c0-51,41.1-92.3,91.9-92.3c38.2,0,70.9,23.4,84.8,56.8C269.8,121.9,302.6,98.4,340.8,98.4 M340.8,83C307,83,276,98.8,256,124.8  c-20-26-51-41.8-84.8-41.8C112.1,83,64,131.3,64,190.7c0,27.9,10.6,54.4,29.9,74.6L245.1,418l10.9,11l10.9-11l148.3-149.8  c21-20.3,32.8-47.9,32.8-77.5C448,131.3,399.9,83,340.8,83L340.8,83z" stroke="#727272" />
+                </svg>
+            </nav>
+            <div className="photo">
+                <img src="https://s-media-cache-ak0.pinimg.com/236x/3b/36/ca/3b36ca3afe0fa0fd4984b9eee2e154bb.jpg" alt="Plant" />
             </div>
-        </body>
+            <div className="description">
+                <h2>Classic Peace Lily</h2>
+                <h4>Popular House Plant</h4>
+                <h1>$18</h1>
+                <p>Classic Peace Lily is a spathiphyllum floor plant arranged in a bamboo planter with a blue & red ribbon and butterfly pick.</p>
+                <button>Add to Cart</button>
+                <button>Wishlist</button>
+            </div>
+        </div>
+        </div>
+
     );
 };
 
@@ -60,30 +42,30 @@ export default function Details() {
 
 
 
-            /* <div>
+/* <div>
 
-        <div>
-            <img src="public/images/w2.jpg"/>  
-        </div>
+<div>
+<img src="public/images/w2.jpg"/>  
+</div>
 
-        <div>
-            <h2>Title: Apple Watch</h2>
-            <img src="public/images/w2.jpg"/>
-            <p>Details: 2020 Apple watch ,hear detection and calling properity</p>
-            <p>Price: 200 J</p>
-            <p>Contact Informations: +962145766324</p>
-        </div>
-
-
-
-
-        </div>
+<div>
+<h2>Title: Apple Watch</h2>
+<img src="public/images/w2.jpg"/>
+<p>Details: 2020 Apple watch ,hear detection and calling properity</p>
+<p>Price: 200 J</p>
+<p>Contact Informations: +962145766324</p>
+</div>
 
 
 
-        <h2>Title: Apple Watch</h2>
-        <img src="public/images/w2.jpg"/>
-        <p>Details: 2020 Apple watch ,hear detection and calling properity</p>
-        <p>Price: 200 J</p>
-        <p>Contact Informations: +962145766324</p> */
+
+</div>
+
+
+
+<h2>Title: Apple Watch</h2>
+<img src="public/images/w2.jpg"/>
+<p>Details: 2020 Apple watch ,hear detection and calling properity</p>
+<p>Price: 200 J</p>
+<p>Contact Informations: +962145766324</p> */
 
