18c70216186f151e11dbb3c784f6370dc3516d70
mahdimalkawi1 <mahdimalkawi66@gmail.com>
2023-08-31 11:21:56 +0000

    details

--- components/details/details.js
+++ components/details/details.js
@@ -1,71 +1,51 @@
+import Navbar from 'components/layout/Navbar';
 import React from 'react';
+
 export default function Details() {
-    return (
-        <div className="addProduct">
+  return (
+    <>
+    < Navbar />
+    <div className="addProduct">
+      <main className="main">
         <div className="card">
-            <nav>
-                <svg className="arrow" version="1.1" viewBox="0 0 512 512" width="512px" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink">
-                    <polygon points="352,115.4 331.3,96 160,256 331.3,416 352,396.7 201.5,256 " stroke="#727272" />
-                </svg>
-                Back to all Plants
-                <svg className="heart" version="1.1" viewBox="0 0 512 512" width="512px" xmlSpace="preserve" stroke="#727272" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink">
-                    <path d="M340.8,98.4c50.7,0,91.9,41.3,91.9,92.3c0,26.2-10.9,49.8-28.3,66.6L256,407.1L105,254.6c-15.8-16.6-25.6-39.1-25.6-63.9  c0-51,41.1-92.3,91.9-92.3c38.2,0,70.9,23.4,84.8,56.8C269.8,121.9,302.6,98.4,340.8,98.4 M340.8,83C307,83,276,98.8,256,124.8  c-20-26-51-41.8-84.8-41.8C112.1,83,64,131.3,64,190.7c0,27.9,10.6,54.4,29.9,74.6L245.1,418l10.9,11l10.9-11l148.3-149.8  c21-20.3,32.8-47.9,32.8-77.5C448,131.3,399.9,83,340.8,83L340.8,83z" stroke="#727272" />
-                </svg>
-            </nav>
-            <div className="photo">
-                <img src="https://s-media-cache-ak0.pinimg.com/236x/3b/36/ca/3b36ca3afe0fa0fd4984b9eee2e154bb.jpg" alt="Plant" />
+          <div className="card__title">
+            <h3 className="h3">Back to all Plants</h3>
+            <div className="icon">
+              <a href="#">
+                <i className="fa fa-chevron-right"></i>
+              </a>
             </div>
-            <div className="description">
-                <h2>Classic Peace Lily</h2>
-                <h4>Popular House Plant</h4>
-                <h1>$18</h1>
-                <p>Classic Peace Lily is a spathiphyllum floor plant arranged in a bamboo planter with a blue & red ribbon and butterfly pick.</p>
-                <button>Add to Cart</button>
-                <button>Wishlist</button>
+          </div>
+          <div className="card__body">
+            <div className="half">
+              <div className="featured_text">
+                <h1>Classic Peace Lily</h1>
+                <p className="sub">Popular House Plant</p>
+                <p className="price">$18</p>
+              </div>
+              <div className="description">
+                <p className="des">
+                  Classic Peace Lily is a spathiphyllum floor plant arranged in a bamboo planter with a blue & red ribbon and butterfly pick.
+                </p>
+              </div>
             </div>
+            <div className="half image">
+              <img src="https://s-media-cache-ak0.pinimg.com/236x/3b/36/ca/3b36ca3afe0fa0fd4984b9eee2e154bb.jpg" alt="Plant" />
+            </div>
+          </div>
+          <div className="card__footer">
+            <div className="recommend">
+              <p>Recommended</p>
+              <h3>Golden Fox</h3>
+            </div>
+            <div className="action">
+              <button className="bo">Add to Cart</button>
+              <button className="boo">Wishlist</button>
+            </div>
+          </div>
         </div>
-        </div>
-
-    );
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* <div>
-
-<div>
-<img src="public/images/w2.jpg"/>  
-</div>
-
-<div>
-<h2>Title: Apple Watch</h2>
-<img src="public/images/w2.jpg"/>
-<p>Details: 2020 Apple watch ,hear detection and calling properity</p>
-<p>Price: 200 J</p>
-<p>Contact Informations: +962145766324</p>
-</div>
-
-
-
-
-</div>
-
-
-
-<h2>Title: Apple Watch</h2>
-<img src="public/images/w2.jpg"/>
-<p>Details: 2020 Apple watch ,hear detection and calling properity</p>
-<p>Price: 200 J</p>
-<p>Contact Informations: +962145766324</p> */
-
+      </main>
+    </div>
+    </>
+  );
+}
--- pages/account.js
+++ pages/account.js
@@ -22,6 +22,7 @@ export default function Account (){
         <>
             <Navbar decodedToken={decodedToken}/>
             <ProfilePage />
+            
         
         </>
     )
