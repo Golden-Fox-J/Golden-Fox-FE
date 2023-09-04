232bd78d34de26e37c184ec4ba53e85db1c5c38c
mahdimalkawi1 <mahdimalkawi66@gmail.com>
2023-09-04 00:03:56 +0000

    changed the about us

--- components/about/about.js
+++ components/about/about.js
@@ -1,161 +1,177 @@
-import moathimg from "./images/moath.png";
-import mahdiimg from "./images/mahdi4-removebg-preview.png";
-import mutazimg from "./images/mutaz.jpg";
-import mothanaimg from "./images/mothana.png";
-import duaaimg from "./images/duaa.png";
-import zekraimg from "./images/zekra.png";
+import React from 'react';
 
-
-import github from "./images/github.png"
-import email from "./images/email.png"
-
-import logo from "./images/logo.png"
-
-
-
-
-export default function About() {
-
+function About() {
     return (
         <>
-            <h1 id="aboutheader">MEET OUR TEAM</h1>
-            <div id="aboutline"></div>
-            <div id="aboutDiv">
-                <div id="moath">
-                    <img className="member" src='https://avatars.githubusercontent.com/u/125549483?v=4' alt="moath"></img>
-                    <h2 class="name">Mo'ath Shakhatreh</h2>
-                    <h4 class="teamstate">Team leader</h4>
-                    <p class="bio">
-                        bio
-                    </p>
+            <div className='body11'>
+                <div className='aboutbody'>
 
-                    <div class="links">
+                    <h1 className="aboutheader">MEET OUR TEAM</h1>
+                    <div className="aboutline1"></div>
+                    <div className="card-row">
+
+                        <div className="person">
+                            <div className="container">
+                                <div className="container-inner">
+                                    <img className="circle" src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTYtYy0wOF8xLWtzaDZtemEzLmpwZw.jpg" alt="Profile Image 1" />
+                                    <img className="img img3" src="https://avatars.githubusercontent.com/u/125549483?v=4" alt="Image 1" />
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="name">Mo'ath Shakhatreh</div>
+                            <div className="title">Software Developer</div>
+                            <div className="links">
                         <a href="mailto: moath.shakhatreh@gmail.com" target="_blank">
-                            <img class="link4" src={email} alt=""></img>
+                            <img className="link4" src="https://www.freepnglogos.com/uploads/email-png/file-email-icon-svg-wikimedia-commons-20.png" alt=""></img>
                         </a>
 
                         <a href="https://github.com/Moath-shakhatreh" target="_blank">
-                            <img class="link4" src={github} alt=""></img>
+                            <img className="link4" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""></img>
                         </a>
                     </div>
-                </div>
+                        </div>
 
-                <div id="mahdi">
-                    <img className="member" src={mahdiimg} alt="mahdi"></img>
-                    <h2 class="name">Mahdi malkawi</h2>
-                    <h4 class="teamstate">Team member</h4>
-                    <p class="bio">
-                        {" "}
-                        bio
-                    </p>
-                    <div class="links">
-                        <a href="mailto:mahdimalkawi98@hotmail.com" target="_blank">
-                            <img class="link4" src={email} alt=""></img>
+                        <div className="person">
+                            <div className="container">
+                                <div className="container-inner">
+                                    <img className="circle" src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTYtYy0wOF8xLWtzaDZtemEzLmpwZw.jpg" alt="Profile Image 3" />
+                                    <img className="img img3" src="https://avatars.githubusercontent.com/u/125888369?v=4" alt="Image 3" />
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="name">Mahdi Malkawi</div>
+                            <div className="title">Software Developer</div>
+                            <div className="links">
+                        <a href="mailto: mahdimalkawi98@hotmail.com" target="_blank">
+                            <img className="link4" src="https://www.freepnglogos.com/uploads/email-png/file-email-icon-svg-wikimedia-commons-20.png" alt=""></img>
                         </a>
 
                         <a href="https://github.com/mahdimalkawi1" target="_blank">
-                            <img class="link4" src={github} alt=""></img>
+                            <img className="link4" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""></img>
                         </a>
                     </div>
-                </div>
+                        </div>
 
-                <div id="mutaz">
-                    <img className="member" src={mutazimg} alt="mutaz"></img>
-                    <h2 class="name">Mutaz Abutaha</h2>
-                    <h4 class="teamstate">Team member</h4>
-                    <p class="bio">
-                        {" "}
-                        bio
-                    </p>
-                    <div class="links">
-                        <a href="mailto:mutazabutaha13@outlook.com" target="_blank">
-                            <img class="link4" src={email} alt=""></img>
+                        <div className="person">
+                            <div className="container">
+                                <div className="container-inner">
+                                    <img className="circle" src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTYtYy0wOF8xLWtzaDZtemEzLmpwZw.jpg" alt="Profile Image 2" />
+                                    <img className="img img3" src="https://avatars.githubusercontent.com/u/125547543?v=4" alt="Image 2" />
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="name">Mutaz Abutaha</div>
+                            <div className="title">Software Developer</div>
+                            <div className="links">
+                        <a href="mailto: mutazabutaha13@outlook.com" target="_blank">
+                            <img className="link4" src="https://www.freepnglogos.com/uploads/email-png/file-email-icon-svg-wikimedia-commons-20.png" alt=""></img>
                         </a>
 
                         <a href="https://github.com/MutazAbutaha" target="_blank">
-                            <img class="link4" src={github} alt=""></img>
+                            <img className="link4" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""></img>
                         </a>
                     </div>
-                </div>
-                <br />
+                            
+                        </div>
+                    </div>
+                    <div className="card-row">
 
-                <div id="mothana">
-                    <img className="member" src={mothanaimg} alt="mothana"></img>
-                    <h2 class="name">Al-Mothana Al-Masri</h2>
-                    <h4 class="teamstate">Team member</h4>
-                    <p class="bio">
-                        bio
-                    </p>
-                    <div class="links">
-                        <a href="mailto:mothana.almasri.99@gmail.com" target="_blank">
-                            <img class="link4" src={email} alt=""></img>
+                        <div className="person">
+                            <div className="container">
+                                <div className="container-inner">
+                                    <img className="circle" src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTYtYy0wOF8xLWtzaDZtemEzLmpwZw.jpg" alt="Profile Image 2" />
+                                    <img className="img img3" src="https://avatars.githubusercontent.com/u/60059328?v=4" alt="Image 2" />
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="name">Al-Mothana Al-Masri</div>
+                            <div className="title">Software Developer</div>
+                            <div className="links">
+                        <a href="mailto: mothana.almasri.99@gmail.com" target="_blank">
+                            <img className="link4" src="https://www.freepnglogos.com/uploads/email-png/file-email-icon-svg-wikimedia-commons-20.png" alt=""></img>
                         </a>
 
                         <a href="https://github.com/almothana-almasri" target="_blank">
-                            <img class="link4" src={github} alt=""></img>
+                            <img className="link4" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""></img>
                         </a>
                     </div>
-                </div>
+                        </div>
 
-                <div id="duaa">
-                    <img className="member" src={duaaimg} alt="duaa"></img>
-                    <h2 class="name">Duaa Melhem </h2>
-                    <h4 class="teamstate">Team member</h4>
-                    <p class="bio">
-                        bio
-                    </p>
-                    <div class="links">
-                        <a href="mailto:Drduaamelhem@gmail.com" target="_blank">
-                            <img class="link4" src={email} alt=""></img>
+                        <div className="person">
+                            <div className="container">
+                                <div className="container-inner">
+                                    <img className="circle" src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTYtYy0wOF8xLWtzaDZtemEzLmpwZw.jpg" alt="Profile Image 2" />
+                                    <img className="img img2" src="https://avatars.githubusercontent.com/u/125805246?v=4" alt="Image 2" />
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="name">Duaa Melhem</div>
+                            <div className="title">Software Developer</div>
+                            <div className="links">
+                        <a href="mailto: Drduaamelhem@gmail.com" target="_blank">
+                            <img className="link4" src="https://www.freepnglogos.com/uploads/email-png/file-email-icon-svg-wikimedia-commons-20.png" alt=""></img>
                         </a>
-
+                        
                         <a href="https://github.com/doaamelhem96" target="_blank">
-                            <img class="link4" src={github} alt=""></img>
+                            <img className="link4" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""></img>
                         </a>
                     </div>
-                </div>
-                <div id="zekra">
-                    <img className="member" src={zekraimg} alt="zekra"></img>
-                    <h2 class="name">Zekra Quraan </h2>
-                    <h4 class="teamstate">Team member</h4>
-                    <p class="bio">
-                        bio
-                    </p>
-                    <div class="links">
-                        <a href="mailto:zekraquraan7@gmail.com" target="_blank">
-                            <img class="link4" src={email} alt=""></img>
+                        </div>
+
+                        <div className="person">
+                            <div className="container">
+                                <div className="container-inner">
+                                    <img className="circle" src="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwMTYtYy0wOF8xLWtzaDZtemEzLmpwZw.jpg" alt="Profile Image 2" />
+                                    <img className="img img2" src="https://avatars.githubusercontent.com/u/125604498?v=4" alt="Image 2" />
+                                    <a href={`mailto:moath.shakhatreh@gmail.com`} className="icon">
+                                    <i className="fas fa-envelope"></i>
+                                </a>
+                                {/* Icon for GitHub */}
+                                <a href="https://github.com/Moath-shakhatreh" className="icon">
+                                    <i className="fab fa-github"></i>
+                                </a>
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="name">Zekra Quraan</div>
+                            <div className="title">Software Developer</div>
+                            <div className="links">
+                        <a href="mailto: zekraquraan7@gmail.com" target="_blank">
+                            <img className="link4" src="https://www.freepnglogos.com/uploads/email-png/file-email-icon-svg-wikimedia-commons-20.png" alt=""></img>
                         </a>
 
                         <a href="https://github.com/zekraquraan" target="_blank">
-                            <img class="link4" src={github} alt=""></img>
+                            <img className="link4" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt=""></img>
                         </a>
                     </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
+                <div className="p1">
+                    <div>
 
-            <h1 id="aboutheader2">About </h1>
-            <div id="aboutline2"></div>
-            {/* <img id="product" src={product} alt=""></img> */}
-            <div id="p1">
-                <img id="logo" src={logo} alt=""></img>
-                <p id="Golden Fox">
-                    An online store <br /> 
-                    an online store where you can explore a vast collection of products and effortlessly make purchases.
-                    Additionally, you have the option to create a curated list of your favorite items for easy reference and future shopping.
+                        <h1 className="aboutheader">Golden Fox</h1>
+                        <div className="aboutline"></div>
+                    </div>
+                    {/* <img className="logo" src="https://scalebranding.com/wp-content/uploads/2022/02/golden-fox.jpg" alt=""></img> */}
+                    <p className='bbb'>An online store <br />
+                        an online store where you can explore a vast collection of products and effortlessly make purchases.
+                        Additionally, you have the option to create a curated list of your favorite items for easy reference and future shopping.</p>
 
-                </p>
-            </div>
-
-            <div id="p2">
-                <h3 id="vision1">Our vision </h3>
-                <p id="vision">
-                "Our vision is to be the ultimate online shopping destination,
-                 offering a wide range of high-quality products while enabling customers to easily create and manage their personalized lists of favorites,
-                  making shopping both convenient and enjoyable."
-                </p>
+                </div>
+                <div className="p2">
+                    <div>
+                        <h1 className="aboutheader">Our vision</h1>
+                        <div className="aboutline"></div>
+                    </div>
+                    <p className='bbb'>Our vision is to be the ultimate online shopping destination,
+                        offering a wide range of high-quality products while enabling customers to easily create and manage their personalized lists of favorites,
+                        making shopping both convenient and enjoyable.</p>
+                </div>
             </div>
         </>
     );
 }
 
+export default About;
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
 import Navbar from "components/layout/Navbar"
 import About from "components/about/about";
+import Footer from "components/layout/Footer";
+import { useState, useEffect } from "react"
 
 
 
@@ -7,14 +9,20 @@ import About from "components/about/about";
 
 export default function AboutPage (){
 
-
+    const [decodedToken, setDecodedToken] = useState()
+    useEffect(() => {
+        if (typeof window !== 'undefined' && window.localStorage) {
+        setDecodedToken(JSON.parse(localStorage.getItem('decodedToken')))
+        }
+    }, []);
 
 
 
     return(
         <>
-            <Navbar />
+            <Navbar decodedToken={decodedToken} />
             <About />
+            <Footer />
         
         </>
     )
